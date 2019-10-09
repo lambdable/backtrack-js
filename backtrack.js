698046ca7a2524106d5df5cad09fe184f04ebc56
Zaim Ramlan <zaem.muz@gmail.com>
2019-10-09 10:54:22 +0800

    clean up codes

--- backtrack.js
+++ backtrack.js
@@ -6,87 +6,95 @@
 class Backtrack {
     /**
      * Initializes an instance of Backtrack.
+     *
+     * @constructor
+     * @author: Zaim Ramlan
+     * @param {string} backButtonId The `id` property of the back button.
      */
-    constructor() {}
+    constructor(backButtonId) {
+        this._backButtonId = backButtonId;
+        this._storageKey = 'backtracks';
+    }
 
     /**
-     * Pushes the current page into the history stack.
+     * Sets the back_link href.
+     *
+     * assumptions:
+     * 1) Back link is a `<a href=""></a>`
+     * 2) Back link has an id of `back_link_id`
+     *
+     * i.e.
+     * <a id="back_button" href="">Back</a>
+     */
+    setBackLink() {
+        var backButton = document.getElementById(this._backButtonId);
+        if (backButton != null) backButton.href = this._getBackButtonURL();
+    }
+
+    /**
+     * Pushes the current page into the history stack if it's not already in.
      */
     pushPage() {
-        pages_from_storage = sessionStorage.getItem('backtracks');
-        if(pages_from_storage == null) {
-            pages = new Array();
-        } else {
-            pages = pages_from_storage.split(',');
+        var storedPages = this._getStoredPages();
+        var isStoredPagesExist = storedPages != null;
+        var pages = isStoredPagesExist ? storedPages.split(',') : new Array();
+
+        var lastStoredIndex = pages.length - 1;
+        var lastStoredPage = pages[lastStoredIndex];
+        var currentPage = location.href;
+
+        if (lastStoredPage != currentPage) {
+            pages.push(currentPage);
+            this._setToStore(pages);
         }
-        /*
-        * if the last page in the history stack is not the same as the current page,
-        * only then we push it into the history stack.
-        *
-        * this condition can happen upon page reload/refresh:
-        * 0. Load page A the first time and it goes into the history stack
-        * 1. Now page A is in history stack
-        * 2. Reload/refresh Page A
-        * 3. Checks if Page A is in the stack (it is!)
-        * 4. Ignores it, otherwise we will have a consequent duplicate entry
-        */
-        if(pages[pages.length-1] != location.href) pages.push(location.href);
-        sessionStorage.setItem('backtracks', pages.toString());
     }
 
     /**
      * Removes the last page in the history stack.
      */
     popPage() {
-        pages_from_storage = sessionStorage.getItem('backtracks');
-        /*
-        * pages are stored as "domain.com/page_a, domain.com/page_b, ..."
-        * split the string and put it as an array into `pages`
-        */
-        pages = pages_from_storage.split(',');
-        // remove the page from the history stack
+        var storedPages = this._getStoredPages();
+        var pages = storedPages.split(',');
         pages.pop();
-        sessionStorage.setItem('backtracks', pages.toString());
+        this._setToStore(pages)
     }
 
     /**
-     * Returns the back link.
+     * Removes all stored pages.
      */
-    backLink() {
-        pages_from_storage = sessionStorage.getItem('backtracks');
-        /*
-        * pages are stored as "domain.com/page_a, domain.com/page_b, ..."
-        * split the string and put it as an array into `pages`
-        */
-        pages = pages_from_storage.split(',');
-        /*
-        * the back link will be the page before the current page.
-        *
-        * actually, it should be `pages.length - 1`, but since
-        * JS array starts from [0], we need to to minus an extra 1
-        */
-        back_link = pages[pages.length - 2];
-        return back_link;
+    clearPages() {
+        sessionStorage.removeItem(this._storageKey);
     }
 
-    /*
-    * Sets the back_link href.
-    *
-    * assumptions:
-    * 1) Back link is a `<a href=""></a>`
-    * 2) Back link has an id of `back_link_id`
-    *
-    * i.e.
-    * <a id="back_button" href="">Back</a>
-    */
-    setBackLink() {
-        // finds the back button with id `back_link_id`
-        back_button = document.getElementById(back_link_id);
-        // if there IS a back button of id `back_link_id`, only then set the back link href
-        if (back_button != null) back_button.href = this.back_link();
+    /**
+     * Returns the page, before the current page, as the link to go back.
+     *
+     * @return {string} The second last stored page's URL for the back button to link to.
+     */
+    _getBackButtonURL() {
+        var storedPages = this._getStoredPages();
+        var pages = storedPages.split(',');
+
+        var secondLastIndex = pages.length - 2
+        var secondLastPage = pages[secondLastIndex];
+        return secondLastPage;
     }
 
-    reset() {
-        sessionStorage.removeItem('backtracks');
+    /**
+     * Get the stored pages.
+     *
+     * @return {array} An array of stored pages.
+     */
+    _getStoredPages() {
+        return sessionStorage.getItem(this._storageKey);
+    }
+
+    /**
+     * Store the given pages array.
+     *
+     * param {array} pages The array of pages to be stored.
+     */
+    _setToStore(pages) {
+        sessionStorage.setItem(this._storageKey, pages.toString());
     }
 }
